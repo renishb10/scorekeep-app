7189c22b39fc551231fbcfb756c14cc5ce4afde0
Renish B <renish@truebase.io>
2019-09-04 19:01:22 +0530

    added more colors

--- client/main.js
+++ client/main.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Meteor } from 'meteor/meteor';
 import { Tracker } from "meteor/tracker";
 
-import { Players } from '../imports/api/players';
+import { Players, calculatePlayerPositions } from '../imports/api/players';
 
 import App from '../imports/ui/components/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
     const players = Players.find({},{sort:{score: -1}}).fetch();
-    ReactDOM.render(<App title="Score Keeper!" players={players}/>, document.getElementById('root'));
+    const positionedPlayers = calculatePlayerPositions(players);
+    ReactDOM.render(<App title="Score Keeper!" players={positionedPlayers}/>, document.getElementById('root'));
   });
 })
--- imports/ui/components/Player.js
+++ imports/ui/components/Player.js
@@ -16,12 +16,15 @@ class Player extends Component {
   };
 
   render() {
+    let itemClassName = `item item--position-${this.props.player.rank}`;
     return (
-      <div key={this.props.player._id} className="item">
+      <div key={this.props.player._id} className={itemClassName}>
         <div className="player">
           <div>
             <h3 className="player__name">{this.props.player.name}</h3>
-            <p className="player__stats">{this.props.player.score} points(s).</p>
+            <p className="player__stats">
+              {this.props.player.rank} {this.props.player.position}  {this.props.player.score} points(s).
+            </p>
           </div>
 
           <div className="player__actions">
